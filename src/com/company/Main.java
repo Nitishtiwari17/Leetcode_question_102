e9d7afa46cd91e54127d5be6a62276cc367c57b2
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2022-10-17 20:12:53 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> level=new ArrayList<>();
+            int cnt=queue.size();
+            for(int i=0;i<cnt;i++){
+                TreeNode node=queue.poll();
+                level.add(node.val);
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
